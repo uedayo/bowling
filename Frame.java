d109ea0a218212afaf41c33d46e6e7ff0dd628c2
Yoshinori Ueda <uedayo@gmail.com>
2012-08-25 00:39:00 +0900

    add add method

--- Frame.java
+++ Frame.java
@@ -1,5 +1,10 @@
-public class Frame{
-  public int getScore(){
+public class Frame {
+  private int itsScore = 0;
+  public int getScore() {
     return 0;
+  }
+
+  public void add(int pins) {
+    itsScore += pins;
   }
 }
--- TestFrame.java
+++ TestFrame.java
@@ -2,12 +2,13 @@ import junit.framework.*;
 
 public class TestFrame extends TestCase {
 
-  public TestFrame(String name){
+  public TestFrame(String name) {
     super(name);
   }
 
-  public void testScoreNoThrows(){
+  public void testScoreNoThrows() {
     Frame f = new Frame();
-    assertEquals(0, f.getScore());
+    f.add(5);
+    assertEquals(5, f.getScore());
   }
 }
