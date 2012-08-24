df3a6fa329e713cb71302269b8d14c077666d8c8
Yoshinori Ueda <uedayo@gmail.com>
2012-08-25 02:29:21 +0900

    add scoreForFrame

--- Game.java
+++ Game.java
@@ -1,12 +1,24 @@
 public class Game {
 
   private int itsScore = 0;
+  private int[] itsThrows = new int[21];
+  private int itsCurrentThrow = 0;
 
   public int score() {
     return itsScore;
   }
 
   public void add(int pins) {
+    itsThrows[itsCurrentThrow++] = pins;
     itsScore += pins;
+  }
+
+  public int scoreForFrame(int frame){
+    int score = 0;
+    for (int ball = 0; frame > 0 && (ball < itsCurrentThrow);
+         ball +=2, frame--){
+      score += itsThrows[ball] + itsThrows[ball+1];
+    }
+    return score;
   }
 }
--- TestGame.java
+++ TestGame.java
@@ -18,4 +18,15 @@ public class TestGame extends TestCase {
     g.add(4);
     assertEquals(9, g.score());
   }
+
+  public void testFourThrowsNoMark(){
+    Game g = new Game();
+    g.add(5);
+    g.add(4);
+    g.add(7);
+    g.add(2);
+    assertEquals(18, g.score());
+    assertEquals(9, g.scoreForFrame(1));
+    assertEquals(18, g.scoreForFrame(2));
+  }
 }
