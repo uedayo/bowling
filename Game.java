3b655520656b2b068a560c04db7bc988e2d4f8cf
Yoshinori Ueda <uedayo@gmail.com>
2012-08-26 03:39:21 +0900

    modify handleSecondThrow

--- Game.java
+++ Game.java
@@ -64,15 +64,22 @@ public class Game {
     
     int frameScore = firstThrow + secondThrow;
 
-    if (frameScore == 10) {
+    if ( spare() ) {
       ball += 2;
-      score += frameScore + itsThrows[ball];
+      score += 10 + nextBall();
     } else {
       ball += 2;
       score += frameScore;
     }
 
     return score;
+  }
+
+  private boolean spare() {
+    return (itsThrows[ball] + itsThrows[ball + 1]) == 10;
+  }
+  private int nextBall() {
+    return itsThrows[ball];
   }
 
   private int ball;
