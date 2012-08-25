2c6c1c2cb95af9b4d70e7559bfccf2a98f98b969
Yoshinori Ueda <uedayo@gmail.com>
2012-08-26 03:14:08 +0900

    add handleSecondThrow

--- Game.java
+++ Game.java
@@ -4,7 +4,7 @@ public class Game {
   private int[] itsThrows = new int[21];
   private int itsCurrentThrow = 0;
   private int itsCurrentFrame = 1;
-  private boolean firstThrow = true;
+  private boolean firstThrowInFrame = true;
 
   public int score() {
     return scoreForFrame(getCurrentFrame() - 1);
@@ -17,15 +17,15 @@ public class Game {
   }
 
   private void adjustCurrentFrame(int pins) {
-    if (firstThrow == true) {
+    if (firstThrowInFrame == true) {
       if (pins == 10) {
         // Strike
         itsCurrentFrame++;
       } else {
-        firstThrow = false;
+        firstThrowInFrame = false;
       }
     } else {
-      firstThrow = true;
+      firstThrowInFrame = true;
       itsCurrentFrame++;
     }
 
@@ -33,28 +33,40 @@ public class Game {
   }
 
   public int scoreForFrame(int theFrame){
-    int ball = 0;
+    ball = 0;
     int score = 0;
 
-    for (int currentFrame = 0; currentFrame < theFrame; currentFrame++){
-      int firstThrow = itsThrows[ball++];
+    for (int currentFrame = 0; currentFrame < theFrame; currentFrame++) {
+      firstThrow = itsThrows[ball++];
 
       if (firstThrow == 10) {
         score += 10 + itsThrows[ball] + itsThrows[ball + 1];
       } else {
-        int secondThrow = itsThrows[ball++];
-        int frameScore = firstThrow + secondThrow;
-
-        if (frameScore == 10) {
-          score += frameScore + itsThrows[ball];
-        } else {
-          score += frameScore;
-        }
+        score += handleSecondThrow();
       }
     }
 
     return score;
   }
+  
+  public int handleSecondThrow() {
+    int score = 0;
+    secondThrow = itsThrows[ball++];
+    
+    int frameScore = firstThrow + secondThrow;
+
+    if (frameScore == 10) {
+      score += frameScore + itsThrows[ball];
+    } else {
+      score += frameScore;
+    }
+
+    return score;
+  }
+
+  private int ball;
+  private int firstThrow;
+  private int secondThrow;
 
   public int getCurrentFrame() {
     return itsCurrentFrame;
